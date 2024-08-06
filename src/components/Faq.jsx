f7606a05f3a71a1f3429bbda3239a5f11498ed2a
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-08-06 10:54:15 +0530

    change spacing

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -2,7 +2,7 @@ import Accordion from "./Accordian"
 
 const Faq = () => {
     return (
-        <div className="container lg:py-[140px] md:py-20 pb-16 pt-0">
+        <div className="container lg:py-[140px] md:py-20 py-16">
             <h1 className=" text-center font-semibold  md:text-5xl text-4xxl  text-black capitalize">Frequently Asked Questions</h1>
             <p className="text-lg text-black-100 max-w-[592px] text-center pt-5 mx-auto">Lörem ipsum koda astrobel: sutaveligen. Rodod bänera viliga. Pregigt primasofi dede facebooka: förutom tivaligt. Fejkade</p>
             <Accordion />
